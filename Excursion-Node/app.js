2dfedb567eaab83b4dfa785274ff33b114c8c5f8
jgellatly5 <jgellatly5@gmail.com>
2017-10-16 20:54:46 -0700

    email working

--- Excursion-Node/app.js
+++ Excursion-Node/app.js
@@ -34,7 +34,31 @@ app.post('/form', (req, res) => {
             email: req.body.email || null
         }));
     }, 1000)
+
     console.log('you posted: Name: ' + req.body.name + ', Email: ' + req.body.email);
+    var nodemailer = require('nodemailer');
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'jgellatly5@gmail.com',
+            pass: 'beloteca1992'
+        }
+    });
+
+    var mailOptions = {
+        from: 'jgellatly5@gmail.com',
+        to: 'jgellatly5@gmail.com',
+        subject: 'Excursion test',
+        text: 'This is a test!'
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 });
 
 app.get('*', (req, res) => {
