a6c40b000cdc17d83ac9c0ebb28bfcdcbb3bf940
Jordan Gellatly <jgellatly5@gmail.com>
2017-11-08 13:02:16 -0800

    formatted buton

--- Excursion-Node/excursion-react/src/components/Screen_1_1.js
+++ Excursion-Node/excursion-react/src/components/Screen_1_1.js
@@ -13,65 +13,67 @@ class Screen_1_1 extends Component{
     }
     render() {
         return (
-            <div className="ad-signup">
-                <Panel className="ad-signup-panel">
-                <h1>Let{`'`}s Get Started</h1>
-                <p>Create Your Account</p>
-                <div>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label className="control-label">First Name</label>
-                            <input
-                                value={this.state.firstName}
-                                onChange={this.onChange}
-                                type="text"
-                                name="firstName"
-                                className="form-control"
-                            />
-                        </div>
+            <div>
+                <div className="ad-signup">
+                    <Panel className="ad-signup-panel">
+                    <h1>Let{`'`}s Get Started</h1>
+                    <p>Create Your Account</p>
+                    <div>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label className="control-label">First Name</label>
+                                <input
+                                    value={this.state.firstName}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="firstName"
+                                    className="form-control"
+                                />
+                            </div>
 
-                        <div className="form-group">
-                            <label className="control-label">Last Name</label>
-                            <input
-                                value={this.state.lastName}
-                                onChange={this.onChange}
-                                type="text"
-                                name="lastName"
-                                className="form-control"
-                            />
-                        </div>
+                            <div className="form-group">
+                                <label className="control-label">Last Name</label>
+                                <input
+                                    value={this.state.lastName}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="lastName"
+                                    className="form-control"
+                                />
+                            </div>
 
-                        <div className="form-group">
-                            <label className="control-label">Email</label>
-                            <input
-                                value={this.state.email}
-                                onChange={this.onChange}
-                                type="text"
-                                name="email"
-                                className="form-control"
-                            />
-                        </div>
+                            <div className="form-group">
+                                <label className="control-label">Email</label>
+                                <input
+                                    value={this.state.email}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="email"
+                                    className="form-control"
+                                />
+                            </div>
 
-                        <div className="form-group">
-                            <label className="control-label">Password</label>
-                            <input
-                                value={this.state.password}
-                                onChange={this.onChange}
-                                type="password"
-                                name="password"
-                                className="form-control"
-                            />
-                        </div>
+                            <div className="form-group">
+                                <label className="control-label">Password</label>
+                                <input
+                                    value={this.state.password}
+                                    onChange={this.onChange}
+                                    type="password"
+                                    name="password"
+                                    className="form-control"
+                                />
+                            </div>
 
-                        <div className="form-group">
-                            <button className="btn btn-info btn-lg">
-                                Next
-                            </button>
-                            {/*<Link className="signup" to="/login">Login to Excursion</Link>*/}
-                        </div>
-                    </form>
+                            <div className="form-group">
+                                <button className="btn btn-info btn-lg">
+                                    Next
+                                </button>
+                                {/*<Link className="signup" to="/login">Login to Excursion</Link>*/}
+                            </div>
+                        </form>
+                    </div>
+                    </Panel>
                 </div>
-                </Panel>
             </div>
         );
     }
