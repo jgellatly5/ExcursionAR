15d8cbecefb71d1959fb7fe6b9ecdc1b96cbbc61
Jordan Gellatly <jgellatly5@gmail.com>
2017-11-10 14:20:17 -0800

    need to figure out how to toggle form

--- Excursion-Node/excursion-react/src/components/Screen_1_1.js
+++ Excursion-Node/excursion-react/src/components/Screen_1_1.js
@@ -14,7 +14,7 @@ class Screen_1_1 extends Component{
     render() {
         return (
             <div className="ad-signup-container">
-                <div className="ad-signup">
+                <div id="hidden" className="ad-signup">
                     <Panel className="ad-signup-panel">
                     <h1>Let{`'`}s Get Started</h1>
                     <p>Create your account.</p>
@@ -60,6 +60,66 @@ class Screen_1_1 extends Component{
                                     onChange={this.onChange}
                                     type="password"
                                     name="password"
+                                    className="form-control"
+                                    id="last"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <button className="btn btn-lg">
+                                    Next
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    </Panel>
+                </div>
+                <div className="ad-signup">
+                    <Panel className="ad-signup-panel">
+                    <h1>What is your business?</h1>
+                    <p>Fill out information about your business.</p>
+                    <div>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label className="control-label">Company Name</label>
+                                <input
+                                    value={this.state.companyName}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="companyName"
+                                    className="form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label className="control-label">Industry</label>
+                                <input
+                                    value={this.state.industry}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="industry"
+                                    className="form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label className="control-label">Phone Number</label>
+                                <input
+                                    value={this.state.phoneNumber}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="phoneNumber"
+                                    className="form-control"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label className="control-label">Website</label>
+                                <input
+                                    value={this.state.website}
+                                    onChange={this.onChange}
+                                    type="text"
+                                    name="website"
                                     className="form-control"
                                     id="last"
                                 />
