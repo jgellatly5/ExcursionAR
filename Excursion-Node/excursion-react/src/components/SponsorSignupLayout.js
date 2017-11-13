11a64095f6e53676c261dbbc4e920931abf6580c
Jordan Gellatly <jgellatly5@gmail.com>
2017-11-12 21:19:42 -0800

    need to figure out leaving transition

--- Excursion-Node/excursion-react/src/components/SponsorSignupLayout.js
+++ Excursion-Node/excursion-react/src/components/SponsorSignupLayout.js
@@ -9,11 +9,19 @@ class SponsorSignupLayout extends Component {
         super(props);
         this.handleScreenChange = this.handleScreenChange.bind(this);
         this.state = {
-            screen: <Screen_1_0 key="0" handler={this.handleScreenChange}/>
+            screen: <Screen_1_0 key="0" handler={this.handleScreenChange}/>,
+            mounted: false,
+            newMounted: false
         };
+    }
+    componentDidMount() {
+        this.setState({
+            mounted: true
+        });
     }
     handleScreenChange(e, num) {
         e.preventDefault();
+        this.setState({ mounted: false, newMounted: true })
         let newScreen = "";
         switch(num) {
             case 1:
@@ -30,6 +38,12 @@ class SponsorSignupLayout extends Component {
         });
     }
     render() {
+        let child;
+        if (this.state.mounted) {
+            child = this.state.screen;
+        } else if (this.state.newMounted) {
+            child = this.state.screen;
+        }
         return(
             <div>
                 <ReactCSSTransitionGroup
@@ -37,7 +51,7 @@ class SponsorSignupLayout extends Component {
                     transitionEnterTimeout={2000}
                     transitionLeaveTimeout={2000}
                 >
-                    {this.state.screen}
+                    {child}
                 </ReactCSSTransitionGroup>
             </div>
         );
