2d1a6e323ac09ea1dac201b4c59f90c0687b1b91
Jordan Gellatly <jgellatly5@gmail.com>
2017-12-03 17:43:22 -0800

    Added back button

--- Excursion-Node/excursion-react/src/components/ChooseService.js
+++ Excursion-Node/excursion-react/src/components/ChooseService.js
@@ -10,18 +10,18 @@ class ChooseService extends Component{
                     <Panel className="ad-signup-panel">
                     <h1>Choose what service you would like to use</h1>
                     <p>Choose from our own ads or create and submit your own.</p>
-                    <div className="container choose-path">
+                    <div className="container choose-service">
                         <Grid>
                             <Row>
                                 <Col xs={6}>
-                                    <Panel className="choose-path-panel">
+                                    <Panel className="choose-service-panel">
                                         <img src={require("../blank.png")} className="icon-speaker"/>
                                         <p className="desc">I am addicted to Chinese food</p>
                                         <p className="desc">I am addicted to Chinese food</p>
                                         <p className="desc">I am addicted to Chinese food</p>
                                     </Panel>
                                 </Col>
-                                <Col xs={6} className="choose-path-panel">
+                                <Col xs={6} className="choose-service-panel">
                                     <Panel>
                                         <img src={require("../blank.png")} className="icon-phone"/>
                                         <p className="desc">I am addicted to Chinese food</p>
@@ -32,6 +32,9 @@ class ChooseService extends Component{
                             </Row>
                         </Grid>
                         <div className="form-group">
+                            <button className="btn btn-lg" ref="button" onClick={this.endScreen}>
+                                Back
+                            </button>
                             <button className="btn btn-lg" ref="button" onClick={this.endScreen}>
                                 Next
                             </button>
