324d7a3f07cd5eff6a1efffce39119928d40c8ee
Jordan Gellatly <jgellatly5@gmail.com>
2017-12-03 22:00:13 -0800

    Complete choose service screen per design of invision
    Created two card like choices for selecting free or premium account.
    Added back button inline with next button.

--- Excursion-Node/excursion-react/src/components/ChooseService.js
+++ Excursion-Node/excursion-react/src/components/ChooseService.js
@@ -5,17 +5,18 @@ import { LinkContainer } from 'react-router-bootstrap';
 class ChooseService extends Component{
     render() {
         return (
-            <div className="ad-signup-container">
+            <div className="choose-service-container">
                 <div className="ad-signup">
                     <Panel className="ad-signup-panel">
-                    <h1>Choose what service you would like to use</h1>
-                    <p>Choose from our own ads or create and submit your own.</p>
-                    <div className="container choose-service">
+                    <h1 className="top-header">Choose what service you would like to use</h1>
+                    <p className="top">Choose from our own ads or create and submit your own.</p>
+                    <div className="container-services choose-service">
                         <Grid>
                             <Row>
                                 <Col xs={6}>
                                     <Panel className="choose-service-panel">
-                                        <img src={require("../blank.png")} className="icon-speaker"/>
+                                        <img src={require("../blank.png")} className="icon-confetti"/>
+                                        <h3 className="header-service">Free</h3>
                                         <p className="desc">I am addicted to Chinese food</p>
                                         <p className="desc">I am addicted to Chinese food</p>
                                         <p className="desc">I am addicted to Chinese food</p>
@@ -23,7 +24,8 @@ class ChooseService extends Component{
                                 </Col>
                                 <Col xs={6} className="choose-service-panel">
                                     <Panel>
-                                        <img src={require("../blank.png")} className="icon-phone"/>
+                                        <img src={require("../blank.png")} className="icon-dollar"/>
+                                        <h3 className="header-service">Premium</h3>
                                         <p className="desc">I am addicted to Chinese food</p>
                                         <p className="desc">I am addicted to Chinese food</p>
                                         <p className="desc">I am addicted to Chinese food</p>
@@ -31,14 +33,14 @@ class ChooseService extends Component{
                                 </Col>
                             </Row>
                         </Grid>
-                        <div className="form-group">
-                            <button className="btn btn-lg" ref="button" onClick={this.endScreen}>
-                                Back
-                            </button>
-                            <button className="btn btn-lg" ref="button" onClick={this.endScreen}>
-                                Next
-                            </button>
-                        </div>
+                    </div>
+                    <div className="bottom-form">
+                        <button className="btn btn-lg back" ref="button" onClick={this.endScreen}>
+                            Back
+                        </button>
+                        <button className="btn btn-lg next" ref="button" onClick={this.endScreen}>
+                            Next
+                        </button>
                     </div>
                     </Panel>
                 </div>
