f7e6510756b19ef47323a3fd93f41475dfbee7a5
Jordan Gellatly <jgellatly5@gmail.com>
2017-12-05 23:57:08 -0800

    Attempt to change className of Panel
    The React Bootstrap component is not allowing the dom to add use the
    classlist add function. I can use setState since the asynchronous call
    isnt that noticable for the change in css but now the css is not
    complying. I just need to work on the css for when the component is
    clicked and is active

--- Excursion-Node/excursion-react/src/components/ChooseService.js
+++ Excursion-Node/excursion-react/src/components/ChooseService.js
@@ -4,14 +4,40 @@ import {Grid, Row, Col, Panel} from 'react-bootstrap';
 class ChooseService extends Component{
     constructor(props) {
         super(props);
+        this.state = {
+            isActive: true,
+            insertClass: "choose-service-panel"
+        };
         this.onSelect = this.onSelect.bind(this);
     }
-    onSelect() {
-        let card = this.refs.card;
-        let button = this.refs.button;
-        console.log(card.classList);
-        console.log(button.classList);
+    onSelect(e) {
+        // let card = this.refs.card;
+        // let button = this.refs.button;
+        // console.log(card.classList);
+        // console.log(button.classList);
         // card.classList.add('active');
+        // let isActive = false;
+        // if (e.onClick()){
+        //     isActive = !isActive;
+        //     return "choose-service-panel active";
+        // } else {
+        //     return "choose-service-panel";
+        // }
+        this.setState({
+            insertClass: "choose-service-panel"
+        });
+        console.log("onSelect");
+        console.log(e.onClick);
+        // if (e.onClick) {
+        //     this.setState({
+        //         insertClass: "choose-service-panel active"
+        //     });
+        //     console.log(this.state.insertClass);
+        // } else {
+        //     this.setState({
+        //         insertClass: "choose-service-panel"
+        //     });
+        // }
     }
     render() {
         return (
@@ -24,7 +50,7 @@ class ChooseService extends Component{
                         <Grid>
                             <Row>
                                 <Col xs={6}>
-                                    <Panel className="choose-service-panel" ref="card" onClick={this.onSelect}>
+                                    <Panel className={this.state.insertClass} ref="card" onClick={this.onSelect}>
                                         <img src={require("../blank.png")} className="icon-confetti"/>
                                         <h3 className="header-service">Free</h3>
                                         <p className="desc">I am addicted to Chinese food</p>
