885966e8903d2c7aa881dae69a40886483b92932
Jordan Gellatly <jgellatly5@gmail.com>
2017-12-07 17:30:33 -0800

    Persist border highlight on selection
    Panel cards stay highlighted when clicked. Next button can be selected
    after a card has been chosen. Each selection fires its own selection
    function.

--- Excursion-Node/excursion-react/src/components/ChooseService.js
+++ Excursion-Node/excursion-react/src/components/ChooseService.js
@@ -6,38 +6,35 @@ class ChooseService extends Component{
         super(props);
         this.state = {
             isActive: true,
-            insertClass: "choose-service-panel"
+            insertCardClass: "choose-service-panel",
+            insertCard2Class: "choose-service-panel"
         };
-        this.onSelect = this.onSelect.bind(this);
+        this.onSelectFree = this.onSelectFree.bind(this);
+        this.onSelectPremium = this.onSelectPremium.bind(this);
     }
-    onSelect(e) {
-        // let card = this.refs.card;
-        // let button = this.refs.button;
-        // console.log(card.classList);
-        // console.log(button.classList);
-        // card.classList.add('active');
-        // let isActive = false;
-        // if (e.onClick()){
-        //     isActive = !isActive;
-        //     return "choose-service-panel active";
-        // } else {
-        //     return "choose-service-panel";
-        // }
+    onSelectFree(e) {
+        let buttonNext = this.refs.buttonNext;
+        let buttonBack = this.refs.buttonBack;
+        buttonNext.classList.add('active', 'hvr-grow');
+        buttonNext.removeAttribute('disabled');
+        buttonBack.classList.add('stable');
         this.setState({
-            insertClass: "choose-service-panel"
+            insertCardClass: "choose-service-panel active",
+            insertCard2Class: "choose-service-panel unactive"
         });
-        console.log("onSelect");
-        console.log(e.onClick);
-        // if (e.onClick) {
-        //     this.setState({
-        //         insertClass: "choose-service-panel active"
-        //     });
-        //     console.log(this.state.insertClass);
-        // } else {
-        //     this.setState({
-        //         insertClass: "choose-service-panel"
-        //     });
-        // }
+    }
+    onSelectPremium(e) {
+        let buttonNext = this.refs.buttonNext;
+        let buttonBack = this.refs.buttonBack;
+        // let freeCard = this.refs.freeCard;
+        // console.log(card);
+        buttonNext.classList.add('active', 'hvr-grow');
+        buttonNext.removeAttribute('disabled');
+        // buttonBack.classList.add('active');
+        this.setState({
+            insertCardClass: "choose-service-panel unactive",
+            insertCard2Class: "choose-service-panel active"
+        });
     }
     render() {
         return (
@@ -50,7 +47,7 @@ class ChooseService extends Component{
                         <Grid>
                             <Row>
                                 <Col xs={6}>
-                                    <Panel className={this.state.insertClass} ref="card" onClick={this.onSelect}>
+                                    <Panel className={this.state.insertCardClass} ref="freeCard" onClick={this.onSelectFree}>
                                         <img src={require("../blank.png")} className="icon-confetti"/>
                                         <h3 className="header-service">Free</h3>
                                         <p className="desc">I am addicted to Chinese food</p>
@@ -59,7 +56,7 @@ class ChooseService extends Component{
                                     </Panel>
                                 </Col>
                                 <Col xs={6}>
-                                    <Panel className="choose-service-panel" ref="card2" onClick={this.onSelect}>
+                                    <Panel className={this.state.insertCard2Class} ref="premiumCard" onClick={this.onSelectPremium}>
                                         <img src={require("../blank.png")} className="icon-dollar"/>
                                         <h3 className="header-service">Premium</h3>
                                         <p className="desc">I am addicted to Chinese food</p>
@@ -71,10 +68,10 @@ class ChooseService extends Component{
                         </Grid>
                     </div>
                     <div className="bottom-form">
-                        <button className="btn btn-lg back hvr-grow" ref="button" onClick={this.endScreen}>
+                        <button className="btn btn-lg back hvr-grow" ref="buttonBack" onClick={this.endScreen}>
                             Back
                         </button>
-                        <button className="btn btn-lg next" ref="button" onClick={this.endScreen}>
+                        <button className="btn btn-lg next" ref="buttonNext" onClick={this.endScreen}>
                             Next
                         </button>
                     </div>
