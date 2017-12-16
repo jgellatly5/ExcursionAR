8c4bdb0e04c30f9336de875b0b9560546f51a4cb
Jordan Gellatly <jgellatly5@gmail.com>
2017-12-16 14:29:21 -0800

    wire up SetBudget component to container

--- Excursion-Node/excursion-react/src/components/SetBudget.js
+++ Excursion-Node/excursion-react/src/components/SetBudget.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Panel } from 'react-bootstrap';
+import { Panel } from 'react-bootstrap';
 import Slider, { Range, createSliderWithTooltip } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
@@ -13,10 +13,9 @@ class SetBudget extends Component{
         this.state = {
             monthlyBudget: ''
         }
-        this.monthlyBudget = 500;
+        this.monthlyBudget = 0;
         this.dailyBudget = 0;
         this.onChange = this.onChange.bind(this);
-        // this.onBudget = this.onBudget.bind(this);
         this.onAfterChange = this.onAfterChange.bind(this);
         this.lastScreen = this.lastScreen.bind(this);
         this.endScreen = this.endScreen.bind(this);
@@ -25,7 +24,6 @@ class SetBudget extends Component{
         let buttonNext = this.refs.buttonNext;
         this.dailyBudget = value;
         this.monthlyBudget = value * 30;
-        console.log(this.monthlyBudget);
         if (value != 0) {
             buttonNext.classList.add('active', 'hvr-grow');
             buttonNext.removeAttribute('disabled');
@@ -39,9 +37,6 @@ class SetBudget extends Component{
         this.setState({
             monthlyBudget: monthlyBudget
         });
-    }
-    onUpdate(){
-        return this.monthlyBudget;
     }
     lastScreen(e) {
         let nextScreen = this.props.screenId - 1;
@@ -61,31 +56,29 @@ class SetBudget extends Component{
             <div className="ad-signup-container">
                 <div className="ad-signup" id="fix-padding">
                     <Panel className="set-budget-panel">
-                    <h1>Tell us your budget</h1>
-                    <p>Set the amount you would like to spend.</p>
-                    <div>
-                        <form>
-                            <div className="form-group">
-                                <label className="control-label">Budget Amount</label>
-                                <SliderWithTooltip
-                                    min={0}
-                                    max={500}
-                                    defaultValue={this.dailyBudget}
-                                    tipFormatter={budgetFormatter}
-                                    tipProps={{ overlayClassName: 'budgetTooltip', prefixCls: 'budgetTooltip' }}
-                                    trackStyle={{ backgroundColor: '#1E1E75', height: 10 }}
-                                    railStyle={{ backgroundColor: '#D8D8D8', height: 10 }}
-                                    onChange={this.onChange}
-                                    onAfterChange={this.onAfterChange}
-                                    ref="slider"
-                                    monthlyBudget={this.monthlyBudget}
-                                    onBudget={this.onBudget}
-                                />
-                                <div className="min-and-max"><p className="min">$0</p><p className="max">$500</p></div>
-                                <div className="month-est"><p>Around ${this.state.monthlyBudget} per month</p></div>
-                                <hr className="budget" />
-                            </div>
-                        </form>
+                        <div className="fix-margin">
+                            <h1>Tell us your budget</h1>
+                            <p>Set the amount you would like to spend.</p>
+                            <form>
+                                <div className="form-group">
+                                    <label className="control-label">Budget Amount</label>
+                                    <SliderWithTooltip
+                                        min={0}
+                                        max={500}
+                                        defaultValue={this.dailyBudget}
+                                        tipFormatter={budgetFormatter}
+                                        tipProps={{ overlayClassName: 'budgetTooltip', prefixCls: 'budgetTooltip' }}
+                                        trackStyle={{ backgroundColor: '#1E1E75', height: 10 }}
+                                        railStyle={{ backgroundColor: '#D8D8D8', height: 10 }}
+                                        onChange={this.onChange}
+                                        onAfterChange={this.onAfterChange}
+                                    />
+                                    <div className="min-and-max"><p className="min">$0</p><p className="max">$500</p></div>
+                                    <div className="month-est"><p>Around ${this.state.monthlyBudget} per month</p></div>
+                                    <hr className="budget" />
+                                </div>
+                            </form>
+                        </div>
                         <div className="bottom-form">
                             <button className="btn btn-lg back hvr-grow" ref="buttonBack" onClick={this.lastScreen}>
                                 Back
@@ -94,7 +87,6 @@ class SetBudget extends Component{
                                 Next
                             </button>
                         </div>
-                    </div>
                     </Panel>
                 </div>
             </div>
