54fd6faadcc9f2658dd10aa88a43ca3769228235
Jordan Gellatly <jgellatly5@gmail.com>
2017-12-27 12:56:06 -0800

    Reducing width of looks great button
    Reducing the width of the bottom button that says looks great has not
    been so simple. When the width is changed, the text continues to be
    aligned in the same place as if the width has not been changed. Going to
    attempt to modify the parent element which is the button to format into
    cell table and that will hopefully allow the text to be modified.

--- Excursion-Node/excursion-react/src/components/ReviewInfo.js
+++ Excursion-Node/excursion-react/src/components/ReviewInfo.js
@@ -5,127 +5,69 @@ class ReviewInfo extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            adType: 'freemium',
-            insertStaticClass: 'freemium-path-panel',
-            insertDynamicClass: 'freemium-path-panel',
-            insertInteractiveClass: 'freemium-path-panel'
+
         };
-        this.adFormat = 0;
-        this.onChange = this.onChange.bind(this);
-        this.onSelectStatic = this.onSelectStatic.bind(this);
-        this.onSelectDynamic = this.onSelectDynamic.bind(this);
-        this.onSelectInteractive = this.onSelectInteractive.bind(this);
-        this.lastScreen = this.lastScreen.bind(this);
-        this.endScreen = this.endScreen.bind(this);
+        // this.lastScreen = this.lastScreen.bind(this);
+        // this.endScreen = this.endScreen.bind(this);
     }
-    onChange(e) {
-        let buttonNext = this.refs.buttonNext;
-        let adName = this.adNameInput.value;
-        let genre = this.genreInput.value;
-        let adFormat = this.adFormat;
-        if (adName !== '' && genre !== '' && adFormat != '0') {
-            buttonNext.classList.add('active', 'hvr-grow');
-            buttonNext.removeAttribute('disabled');
-        } else {
-            buttonNext.classList.remove('active', 'hvr-grow');
-            buttonNext.setAttribute('disabled','disabled');
-        }
-    }
-    onSelectStatic(e) {
-        e.preventDefault();
-        this.adFormat = this.refs.staticAd.props.eventKey;
-        this.setState({
-            adFormat: 'static',
-            insertStaticClass: 'freemium-path-panel active',
-            insertDynamicClass: 'freemium-path-panel',
-            insertInteractiveClass: 'freemium-path-panel'
-        });
-        this.onChange(e);
-    }
-    onSelectDynamic(e) {
-        e.preventDefault();
-        this.adFormat = this.refs.dynamicAd.props.eventKey;
-        this.setState({
-            adFormat: 'dynamic',
-            insertStaticClass: 'freemium-path-panel',
-            insertDynamicClass: 'freemium-path-panel active',
-            insertInteractiveClass: 'freemium-path-panel'
-        });
-        this.onChange(e);
-    }
-    onSelectInteractive(e) {
-        e.preventDefault();
-        this.adFormat = this.refs.interactiveAd.props.eventKey;
-        this.setState({
-            adFormat: 'interactive',
-            insertStaticClass: 'freemium-path-panel',
-            insertDynamicClass: 'freemium-path-panel',
-            insertInteractiveClass: 'freemium-path-panel active'
-        });
-        this.onChange(e);
-    }
-    lastScreen(e) {
-        let nextScreen = this.props.screenId - 1;
-        this.props.handler(e, nextScreen);
-    }
-    endScreen(e) {
-        let nextScreen = this.props.screenId + 1;
-        this.props.handler(e, nextScreen);
-    }
-    componentDidMount() {
-        let buttonNext = this.refs.buttonNext;
-        buttonNext.setAttribute('disabled','disabled');
-    }
+    // lastScreen(e) {
+    //     let nextScreen = this.props.screenId - 1;
+    //     this.props.handler(e, nextScreen);
+    // }
+    // endScreen(e) {
+    //     let nextScreen = this.props.screenId + 1;
+    //     this.props.handler(e, nextScreen);
+    // }
     render() {
         return (
             <div className="ad-signup-container">
                 <div className="ad-signup" id="fix-margin">
                     <Panel className="ad-signup-panel">
-                    <h1>Review your ad</h1>
-                    <p>Review your ad and your settings</p>
-                    <div>
-                        <form>
-                            <Grid>
-                                <Row>
-                                    <Col xs={6}>
-                                        <div className="form-group">
-                                            <label className="control-label">Ad Preview</label>
-                                            <img src={require("../phonePreview.png")}/>
-                                        </div>
-                                    </Col>
-                                    <Col xs={6}>
-                                        <div className="form-group">
-                                            <label className="control-label">Company/Product Name</label>
-                                            <p className="ad-type">Hooli</p>
-                                        </div>
+                        <div className="review-ad-container">
+                            <h1>Review your ad</h1>
+                            <p>Review your ad and your settings</p>
+                            <form>
+                                <Grid>
+                                    <Row>
+                                        <Col xs={6}>
+                                            <div className="form-group">
+                                                <label className="control-label">Ad Preview</label>
+                                                <img src={require("../phonePreview.png")}/>
+                                            </div>
+                                        </Col>
+                                        <Col xs={6}>
+                                            <div className="form-group">
+                                                <label className="control-label">Company/Product Name</label>
+                                                <p className="ad-type">Hooli</p>
+                                            </div>
 
-                                        <div className="form-group">
-                                            <label className="control-label">Ad Type</label>
-                                            <p className="ad-type">Freemium</p>
-                                        </div>
+                                            <div className="form-group">
+                                                <label className="control-label">Ad Type</label>
+                                                <p className="ad-type">Freemium</p>
+                                            </div>
 
-                                        <div className="form-group">
-                                            <label className="control-label">App Environment</label>
-                                            <p className="ad-type">Indoors</p>
-                                        </div>
+                                            <div className="form-group">
+                                                <label className="control-label">App Environment</label>
+                                                <p className="ad-type">Indoors</p>
+                                            </div>
 
-                                        <div className="form-group">
-                                            <label className="control-label">Budget Amount</label>
-                                            <p className="ad-type">$25 per day</p>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Grid>
-                        </form>
-                        <div className="bottom-form">
+                                            <div className="form-group">
+                                                <label className="control-label">Budget Amount</label>
+                                                <p className="ad-type">$25 per day</p>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Grid>
+                            </form>
+                        </div>
+                        <div className="bottom-form review-ad-bottom">
                             <button className="btn btn-lg back hvr-grow" ref="buttonBack" onClick={this.lastScreen}>
                                 Back
                             </button>
-                            <button className="btn btn-lg next" ref="buttonNext" onClick={this.endScreen}>
-                                Next
+                            <button className="btn btn-lg looks-great active hvr-grow" ref="buttonNext" onClick={this.endScreen}>
+                                <div className="looks-great-text align-middle">Looks Great</div>
                             </button>
                         </div>
-                    </div>
                     </Panel>
                 </div>
             </div>
